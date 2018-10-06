6f36bf244d350c975d34b1f163f20a0a69354d20
Eric Mailman <emailman@dtcc.edu>
2018-10-06 10:54:02 -0400

    First release

---
+++ OOP-3/Account3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOP_3
+{
+    class Account3
+    {
+        public string Name { get; }
+        public decimal Balance { get; private set; }
+
+        public Account3(string acctName, decimal acctBalance = 0)
+        {
+            Name = acctName;
+            Balance = acctBalance;
+        }
+
+        public void Deposit(decimal amt)
+        {
+            Balance += amt;
+        }
+
+        public void Withdraw(decimal amt)
+        {
+            if (Balance - amt >= 0)
+                Balance -= amt;
+            else
+                throw new ArgumentOutOfRangeException("Can't withdaw more than balance");
+        }
+
+        override public string ToString()
+        {
+            return $"Account: {Name} / Balance = {Balance:C}";
+        }
+    }
+}
--- OOP-3/Program.cs
+++ OOP-3/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOP_3
 {
@@ -10,6 +7,55 @@ namespace OOP_3
     {
         static void Main(string[] args)
         {
+            // Create a list of accounts
+            var accounts = new List<Account3>
+            {
+                new Account3("Dopey", 500),
+                new Account3("Sleepy"),
+                new Account3("Sneezy", 300)
+            };
+
+            Reports(accounts, "Opening Report");
+
+            // Do some transactions
+            accounts[0].Withdraw(150);
+            Console.WriteLine($"{accounts[0].Name}: Withdraw $150");
+
+            accounts[1].Deposit(100);
+            Console.WriteLine($"{accounts[1].Name}: Deposit $100");
+
+            try
+            {
+                accounts[2].Withdraw(375);
+                Console.WriteLine($"{accounts[2].Name}: Withdraw $375");
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine($"{accounts[2].Name}: {e.ParamName}");
+                Console.WriteLine($"Requested withdrawl: $375.00 / Account Balance: {accounts[2].Balance:C}");
+            }
+
+            Reports(accounts, "\nClosing Report");
+        }
+
+        static void Reports(List<Account3> accts, string heading)
+        {
+            Console.WriteLine(heading);
+            Console.WriteLine("--------------");
+            Console.WriteLine("List of accounts and balances:");
+            foreach (var acct in accts)
+                Console.WriteLine(acct);
+            Console.WriteLine();
+
+            Console.WriteLine("List of account names:");
+            foreach (var acct in accts)
+                Console.WriteLine(acct.Name);
+
+            Console.Write("\nTotal Balance = ");
+            decimal balance = 0;
+            foreach (var acct in accts)
+                balance += acct.Balance;
+            Console.WriteLine($"{balance:C}\n");
         }
     }
 }
